4780c1c9003857b84d82e1c4c1414679ab00fc71
MarksNewHat98 <marksnewhat98@gmail.com>
2026-09-16 17:47:12 +0200

    last commit i pushed nothing bro :sob:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,26 @@
 local map = vim.keymap.set
+local function exec_cmd(cmd, ret)
+    return function()
+        vim.cmd(cmd)
+        return ret
+    end
+end
 
-map({ "i", "n" }, "<C-BS>", "<C-W>")
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>")
+local all_modes = { "i", "x", "n", "s", "c" }
+
+local function table_clone_insert(t, ...)
+    local nt = {}
+    for i, v in pairs(t) do
+        nt[i] = v
+    end
+    for i, v in pairs({...}) do
+        table.insert(nt,  v)
+    end
+    return nt
+end
+
+map(table_clone_insert(all_modes, "t"), "<C-BS>", "<C-W>")
+map(all_modes, "<C-s>", exec_cmd("write"))
 
 map("n", "<leader>fn", "<cmd>enew<cr>")
 
@@ -11,17 +30,13 @@ map("x", ">", ">gv")
 map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
 map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
 
-map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>ba", "<cmd>enew<cr>", { desc = "New File" })
 
-map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
-
-map("n", "<leader>sd", vim.diagnostic.open_float)
 map("n", "<leader>bd", function()
     Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
 
-map({ "i", "n", "s" }, "<esc>", function()
-    vim.cmd("noh")
-    return "<esc>"
-end, { expr = true, desc = "Escape and Clear hlsearch" })
+map(all_modes, "<esc>", exec_cmd("noh", "<esc>"), { expr = true, desc = "Escape and Clear hlsearch" })
 
+map(table_clone_insert(all_modes, "t"), "<C-r>", exec_cmd("redo"))
+map(all_modes, "<C-z>", exec_cmd("undo"))
